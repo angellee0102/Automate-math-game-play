26127c9011912611f69860fc7988c365f7371b95
Angel Lee <fangyulee0102@gmail.com>
2019-11-17 20:27:19 +0800

    solution works, but too slow

--- test.py
+++ test.py
@@ -7,15 +7,19 @@ import numpy as np
 import pyautogui
 
 images = glob.glob("pic/numbers/*.JPG")
+equal_pic= cv2.imread ("pic/equal.JPG")
+answer_box_pic=cv2.imread("pic/answer_box.JPG")
+
 img_array=[]
 for img in images:
     image = cv2.imread (img)
     img_array.append (image)
+
 region_equal=(100,270,750,400)
 
 while True:
-    equal = snp.locateOnScreen('pic\\equal.JPG',region=region_equal)
-    answer_box=snp.locateOnScreen('pic\\answer_box.JPG',region=region_equal)
+    equal = snp.locateOnScreen(equal_pic,region=region_equal)
+    answer_box=snp.locateOnScreen(answer_box_pic,region=region_equal)
     if equal != None and answer_box!= None:
         # print('equal',equal)
         equal_list = list(equal)
@@ -34,65 +38,69 @@ while True:
         is_left_number_two_digit=False
         left_digit=None
         for i in range(0,10):
-            number_path='pic\\numbers\\'+str(i)+'.JPG'
-            left_number_left_digit = snp.locateOnScreen(number_path,region=region_left_number_left,threshold=0.84)
+            # number_path='pic\\numbers\\'+str(i)+'.JPG'
+            left_number_left_digit = snp.locateOnScreen(img_array[i],region=region_left_number_left,threshold=0.84)
             if left_number_left_digit!=None:
                 is_left_number_two_digit=True
                 left_digit=i
-            left_number_right_digit = snp.locateOnScreen(number_path,region=region_left_number_right,threshold=0.84)
+            left_number_right_digit = snp.locateOnScreen(img_array[i],region=region_left_number_right,threshold=0.84)
             if left_number_right_digit!=None:
                 right_digit=i
 
         if is_left_number_two_digit==True: 
             left_number=left_digit*10+right_digit
-            print('左邊數字',left_number)
+            # print('左邊數字',left_number)
         if is_left_number_two_digit==False:
             left_number=right_digit
-            print('左邊數字:', left_number)
+            # print('左邊數字:', left_number)
 
 
         # 中間數字
-        region_middle_number=(equal_x-95,equal_y-20,60,100)
+        region_middle_number=(equal_x-95,equal_y-20,60,85)
         for i in range(0,10):
-            number_path='pic\\numbers\\'+str(i)+'.JPG'
-            middle_digit = snp.locateOnScreen(number_path,region=region_middle_number,threshold=0.84)
+            # number_path='pic\\numbers\\'+str(i)+'.JPG'
+            middle_digit = snp.locateOnScreen(img_array[i],region=region_middle_number,threshold=0.84)
             if middle_digit!=None:
                 middle_number=i
-                print('中間數字:',middle_number)
+                break
+                # print('中間數字:',middle_number)
         
         # 右邊數字     
-        region_right_number_left=(equal_x+85,equal_y-10,55,75)
-        print(region_right_number_left)
-        region_right_number_right=(equal_x+140,equal_y-10,55,75)
-        print(region_right_number_right)
+        region_right_number_left=(equal_x+95,equal_y-15,55,85)
+        # print('region_right_number_left',region_right_number_left)
+        region_right_number_right=(equal_x+140,equal_y-17,55,85)
+        # print('region_right_number_right',region_right_number_right)
         is_right_number_two_digit=False
         right_left_digit=None
+        right_right_digit=None
         for i in range(0,10):
-            number_path='pic\\numbers\\'+str(i)+'.JPG'
-            right_number_right_digit = snp.locateOnScreen(number_path,region=region_right_number_right,threshold=0.87)
+            # number_path='pic\\numbers\\'+str(i)+'.JPG'
+            right_number_right_digit = snp.locateOnScreen(img_array[i],region=region_right_number_right,threshold=0.87)
             # 右邊的位元
-            if right_number_right_digit!=None:
+            if right_number_right_digit!=None and right_right_digit==None:
                 is_right_number_two_digit=True
                 right_right_digit=i
-            right_nunmber_left_digit=snp.locateOnScreen(number_path,region=region_right_number_left,threshold=0.87)
+            right_nunmber_left_digit=snp.locateOnScreen(img_array[i],region=region_right_number_left,threshold=0.87)
             # 左邊的位元
-            if right_nunmber_left_digit!=None:
+            if right_nunmber_left_digit!=None and right_left_digit==None:
                 right_left_digit=i
         if is_right_number_two_digit==True: 
-            print(right_left_digit,right_right_digit)
+            # print(right_left_digit,right_right_digit)
             right_number=right_left_digit*10+right_right_digit
-            print('右邊數字',right_number)
+            # print('右邊數字',right_number)
         if is_right_number_two_digit==False:
             right_number=right_left_digit
-            print('右邊數字:', right_number)
-
+            # print('右邊數字:', right_number)
+        
+        print(left_number, middle_number, right_number)
         # Calculation
-        if (left_number/right_number==middle_number):# 除法
-            pyautogui.click(answer_box_x+150,answer_box_y+270)
-        elif(right_number/middle_number==left_number):# 乘法
+        if (left_number//middle_number==right_number):# 除法
             pyautogui.click(answer_box_x+270,answer_box_y+270)
+        elif(left_number*middle_number==right_number):# 乘法
+            pyautogui.click(answer_box_x+150,answer_box_y+270)
         elif(right_number-middle_number==left_number):# 加號
             pyautogui.click(answer_box_x-150,answer_box_y+270)
         else:# 減號 
             pyautogui.click(answer_box_x,answer_box_y+270)
+
 
